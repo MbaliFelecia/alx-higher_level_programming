b178db434a38fee4eafe3dd33acb1200b02e6204
MbaliFelecia <ramakgashambali@gmail.com>
2023-08-08 01:39:23 -0700

    script that imports a file

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,2 +1,10 @@
 #!/bin/bash
-import this
+zen_of_python = """
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+...
+Namespaces are one honking great idea -- let's do more of those!
+"""
+print(zen_of_python)
