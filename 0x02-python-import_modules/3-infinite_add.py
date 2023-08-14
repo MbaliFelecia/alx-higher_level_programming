39b5d21f399b2d1bfebd455c7d4e301080b61514
MbaliFelecia <ramakgashambali@gmail.com>
2023-08-14 04:54:03 -0700

    modified to print 0 when the name of the program is passed as the only argement

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "___main__":
     import sys
     result = 0
     for arg in sys.argv:
-        if arg != sys.argv[0]:
+        if arg != sys.argv[1:]:
             result += int(arg)
     print(result)
