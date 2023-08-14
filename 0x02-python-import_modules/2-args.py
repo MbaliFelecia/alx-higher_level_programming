6388630d630c03b6edf3684c993cfb3f4638d075
MbaliFelecia <ramakgashambali@gmail.com>
2023-08-14 05:21:43 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     i = len(sys.argv) - 1
 
-    if i == 1:
-        print("{} arguments:".format(i))
-    elif i == 0:
+    if i == 0:
         print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
 
