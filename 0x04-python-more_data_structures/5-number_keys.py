70f073dc03cf3a1613a330feb3e3e94aa1a41bb7
MbaliFelecia <ramakgashambali@gmail.com>
2023-08-17 04:51:49 -0700

    alinment issues fixed

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,7 +3,7 @@ def number_keys(a_dictionary):
     num = 0
     list_keys = list(a_dictionary.keys())
 
-        for i in list_keys:
-            num += 1
+    for i in list_keys:
+        num += 1
 
-        return(num)
+    return (num)
