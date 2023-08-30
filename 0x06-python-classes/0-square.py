6a81b281f91752f87a5419908c41de5bed423fce
MbaliFelecia <ramakgashambali@gmail.com>
2023-08-29 17:37:21 -0700

    update to code

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Defination of empty class 'Square'"""
+
+
 class Square:
     """Class Square that defines a square
     """
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defination of class 'Square' with private instance attribute - size"""
+
+
 class Square:
     """ Class Square that defines a square object
     """
