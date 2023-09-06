8b52dcbea305e6ac53b1f3f7111fceed27d5ad0c
MbaliFelecia <ramakgashambali@gmail.com>
2023-09-06 14:35:05 -0700

    checks the bigger Rectangle by comparing areas of rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -127,7 +127,7 @@ class Rectangle:
         print("Bye rectangle...")
 
     @staticmethod
-    def def bigger_or_equal(rect_1, rect_2):
+    def bigger_or_equal(rect_1, rect_2):
         """ Method which returns the bigger Rectangle
 
         Args:
