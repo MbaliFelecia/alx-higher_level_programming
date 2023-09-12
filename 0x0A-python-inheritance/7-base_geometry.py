60c1aeeecd565cbadf64a4918f84bbd3fe5d82a2
MbaliFelecia <ramakgashambali@gmail.com>
2023-09-12 15:01:14 -0700

    fixing missing closing bracket

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,8 +6,10 @@ class BaseGeometry:
     """ Class that defines the attributes of Geometric Shape """
 
     def area(self):
-        """ Method that defines the area of a geomtric shape """
-
+        """ Method that defines the area of a geomtric shape
+        Raises:
+            Exception: Always
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -16,10 +18,11 @@ class BaseGeometry:
         Args:
             name: name of the object
             value: value of the property
-
+        Raises:
+            TypeError: if value is not integer
+            ValueError: if value is less or equal to 0
         """
-
         if type(value) is not int:
             raise TypeError("{} must be an interger".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name)
+            raise ValueError("{} must be greater than 0".format(name))
