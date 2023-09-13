3eaff84a49fba0c9a1cd4e43d7320dfa78cb85f9
MbaliFelecia <ramakgashambali@gmail.com>
2023-09-13 04:56:44 -0700

    a function that writes an Object to a text file, using a JSON representation

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -8,7 +8,7 @@ def from_json_string(my_str):
     """ A function that returns the python representation of a json string
 
     Args:
-        my_str: the str to be converted 
+        my_str: the str to be converted
     Returns:
         the python representation of the string
     """
