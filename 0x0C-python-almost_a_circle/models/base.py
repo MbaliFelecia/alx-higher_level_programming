7ed5fae7bbcb65f4104bb91a3f36c7606a16e673
MbaliFelecia <ramakgashambali@gmail.com>
2023-09-15 02:11:18 -0700

    Base class with private class attribute __nb_objects.     Constructor __init__ assigns id if provided, or increments __nb_objects for id.

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module  with a class Base """
+class Base:
+    """ Class Base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Intializes instance """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
