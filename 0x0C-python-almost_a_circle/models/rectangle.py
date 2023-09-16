f3e59638a3f9121eb520cbb37eb4098f14c487b5
MbaliFelecia <ramakgashambali@gmail.com>
2023-09-16 01:35:26 -0700

    Update the class Rectangle by updating the public method def update(self, *args): by changing the prototype to update(self, *args, **kwargs) that assigns a key/value argument to attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,8 +82,39 @@ class Rectangle(Base):
         """
         Display the Rectangle instance using '#' characters
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print('#' * self.width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.__width}/{self.__height}"
+
+    def update(self, *args, **kwargs):
+        """
+        Update the attributes of the Rectangle instance using *args and **kwargs.
+
+        Args:
+            *args: Arguments to assign to the atttributes in order:
+                1st argument: id attribute
+                2nd argument: width attribute
+                3rd argument: height attribute
+                4th argument: x attribute
+                5th argument: y attribute
+            **kwargs: Key-value arguments to assign to the attributes.
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
