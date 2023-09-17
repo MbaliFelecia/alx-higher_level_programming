71a7def0950163b153a3a70019f0a851451ee638
MbaliFelecia <ramakgashambali@gmail.com>
2023-09-17 10:00:56 -0700

    update to the square file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,41 +1,73 @@
 #!/usr/bin/python3
-""" A method that inherates a rectangle class and creates a squre """
-
-
+""" Method which contains class square,
+inheritance of class Rectangle
+"""
 from models.rectangle import Rectangle
 
-class Square(Rectangle):
-    def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializing a square instance.
 
-        Args:
-            size (int): The size of the square (width and height)
-            x (int, optional): The x-coordinate of the square.
-            y (int, optional): The y-coordinate of the square.
-            id (int, optional): The ID for the instance.
-        """
+class Square(Rectangle):
+    """ Class Rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializing a square instance """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """
-        Return a formatted string representation of the Square instance.
+        """ str special method """
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
 
-        Returns:
-            str: The formatted string.
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return str_square + str_id + str_xy + str_wh
+
+    @property
+    def size(self):
+        """ getter size """
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """ setter size """
+        self.width = value
+        self.heigh = value
+
+    def __str__(self):
+        """ str special method """
+        str_rectangle = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
+
+        return str_rectangle + str_id + str_xy + str_size
+
+    def update(self, *args, **kwargs):
+        """ update method """
+        if args in not None and len(args) is not 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
-        """
-        Return a dictionary representation of the square instance.
+        """ Return a dictionar with attributes """
+        list_atr = ['id', 'size', 'x', 'y']
+        dict_res = {}
 
-        Returns:
-            dict: A dictionary containing the attributes of a square.
-        """
-        return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
-        }
+        for key in list_atr:
+            if key == 'size':
+                dict_res[key] = getattr(self, 'width')
+            else:
+                dict_res[key] = getattr(self, key)
+
+        return dict_res
