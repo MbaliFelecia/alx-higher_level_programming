a8e5f34c1bb918e88a9676dab358528758608aba
MbaliFelecia <ramakgashambali@gmail.com>
2023-11-18 09:12:09 -0800

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument safe from safe from MySQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -19,8 +19,7 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states\
-                 WHERE name = %s;", argv[4])
+    cur.execute("SELECT * FROM statesWHERE name = %s;", argv[4])
     states = cur.fetchall()
 
     for state in states:
