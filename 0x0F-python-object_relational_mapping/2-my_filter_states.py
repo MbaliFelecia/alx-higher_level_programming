4013cf0336b76bc68cee4f824df69e5e2c322800
MbaliFelecia <ramakgashambali@gmail.com>
2023-11-18 06:59:04 -0800

    takes an argument and prints the states that matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that takes an argument and
+all states values that match it
+from the database `hbtn_0e_0_usa`
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                 WHERE name LIKE BINARY '{}'\
+                 ORDER BY states.id ASC".format(argv[4]))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
