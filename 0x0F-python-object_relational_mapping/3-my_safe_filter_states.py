f7b2b1af17bb3c7ccd7a276e5bd505363ad98975
MbaliFelecia <ramakgashambali@gmail.com>
2023-11-18 07:24:13 -0800

    takes an argument and prints the states that matches the argument safe from MySQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that takes an argument and
+all states values that match it
+from the database `hbtn_0e_0_usa`
+safe from MySQL injections
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("""
+        SELECT
+            *
+        FROM
+            states
+        WHERE
+            name LIKE BINARY %(name)s
+        ORDER BY
+            states.id ASC
+    """, {
+        'name':argv[4]
+    })
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
