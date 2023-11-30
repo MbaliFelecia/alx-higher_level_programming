2b18a28ec08f54bbf62024e43c1657ba0d1e1c1c
MbaliFelecia <ramakgashambali@gmail.com>
2023-11-30 15:59:48 -0800

    find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,7 +14,7 @@ def find_peak(list_of_integers):
         return li[mid]
     elif mid -1 < 0:
         return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
-    elif mid + 1 >= lenght:
+    elif mid + 1 >= length:
         return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
 
     if li[mid - 1] < li[mid] > li[mid + 1]:
