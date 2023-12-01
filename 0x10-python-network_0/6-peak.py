1f1c0c2492f06325d4fd588f62b6a8c18fa6f0e2
MbaliFelecia <ramakgashambali@gmail.com>
2023-11-30 16:04:52 -0800

    fixing spelling

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,7 +10,7 @@ def find_peak(list_of_integers):
     mid = int(length / 2)
     li = list_of_integers
 
-    if mid - 1 < 0 and mid + 1 >= lenght:
+    if mid - 1 < 0 and mid + 1 >= length:
         return li[mid]
     elif mid -1 < 0:
         return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
